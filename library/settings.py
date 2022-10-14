c748785fb918251d56c0de859e58eb06e728c44c
Amy Gori <amydgori@gmail.com>
2022-10-14 09:56:29 -0400

    Fix typeerror

--- library/settings.py
+++ library/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = [BASE_DIR / "staticfiles"]
+    STATIC_ROOT = BASE_DIR / "staticfiles"
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
