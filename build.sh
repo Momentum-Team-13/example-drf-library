69d9e2934a1ba3c2a5bc2ed2addb24df1d91fec6
Amy Gori <amydgori@gmail.com>
2022-10-13 23:02:05 -0400

    Create virtualenv on render

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pipenv install
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+pipenv run python manage.py collectstatic --no-input
+pipenv run python manage.py migrate
